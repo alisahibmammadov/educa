78aa6b8b87babfa921602148ac58d1aa7ff2a299
alisahibmammadov <ali.yek.10@mail.ru>
2023-11-01 14:17:09 +0400

    msg

---
+++ src/data/footer/footerData.js
@@ -0,0 +1,212 @@
+const footerDataAz = [
+  {
+    id: 1,
+    info: [
+      {
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nobis?",
+        linkName: "Read More",
+        iClas: "fa-solid fa-arrow-right",
+        link: "/",
+      },
+    ],
+    pageLink: [
+      { linkName: "Graduation", link: "/" },
+      { linkName: "Admissions", link: "/" },
+      { linkName: "International", link: "/" },
+      { linkName: "FAQs", link: "/" },
+      { linkName: "Courses", link: "/" },
+      { linkName: "About Us", link: "/" },
+      { linkName: "Bookstore", link: "/" },
+      { linkName: "Alumni", link: "/" },
+    ],
+    contact: [
+      {
+        street: "1107 Wood Street Saginaw, MI New York 48607",
+        tel: "+12 (34) 214 280 2000",
+        email: "support@educa.com",
+      },
+    ],
+    subscribe: [
+      {
+        info: "Subsrcibe to our newsletter for latest updates about our site for universe.",
+        placeHolder: "Email Address...",
+        button: "SUBSCRIBE",
+      },
+    ],
+  },
+];
+const footerDataEn = [
+  {
+    id: 1,
+    info: [
+      {
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nobis?",
+        linkName: "Read More",
+        iClas: "fa-solid fa-arrow-right",
+        link: "/",
+      },
+    ],
+    pageLink: [
+      { linkName: "Graduation", link: "/" },
+      { linkName: "Admissions", link: "/" },
+      { linkName: "International", link: "/" },
+      { linkName: "FAQs", link: "/" },
+      { linkName: "Courses", link: "/" },
+      { linkName: "About Us", link: "/" },
+      { linkName: "Bookstore", link: "/" },
+      { linkName: "Alumni", link: "/" },
+    ],
+    contact: [
+      {
+        street: "1107 Wood Street Saginaw, MI New York 48607",
+        tel: "+12 (34) 214 280 2000",
+        email: "support@educa.com",
+      },
+    ],
+    subscribe: [
+      {
+        info: "Subsrcibe to our newsletter for latest updates about our site for universe.",
+        placeHolder: "Email Address...",
+        button: "SUBSCRIBE",
+      },
+    ],
+  },
+];
+const footerDataRu = [
+  {
+    id: 1,
+    info: [
+      {
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, nobis?",
+        linkName: "Read More",
+        iClas: "fa-solid fa-arrow-right",
+        link: "/",
+      },
+    ],
+    pageLink: [
+      { linkName: "Graduation", link: "/" },
+      { linkName: "Admissions", link: "/" },
+      { linkName: "International", link: "/" },
+      { linkName: "FAQs", link: "/" },
+      { linkName: "Courses", link: "/" },
+      { linkName: "About Us", link: "/" },
+      { linkName: "Bookstore", link: "/" },
+      { linkName: "Alumni", link: "/" },
+    ],
+    contact: [
+      {
+        street: "1107 Wood Street Saginaw, MI New York 48607",
+        tel: "+12 (34) 214 280 2000",
+        email: "support@educa.com",
+      },
+    ],
+    subscribe: [
+      {
+        info: "Subsrcibe to our newsletter for latest updates about our site for universe.",
+        placeHolder: "Email Address...",
+        button: "SUBSCRIBE",
+      },
+    ],
+  },
+];
+const footerBottomAz = [
+  {
+    copyRight: "@ COPYRIGHT 2015 EDUCA. ALL RIGHTS RESERVED",
+    pagesLink: [
+      {
+        linkName: "HOME",
+        link: "/",
+      },
+      {
+        linkName: "COURSES",
+        link: "/",
+      },
+      {
+        linkName: "FUTURE STUDENTS",
+        link: "/",
+      },
+      {
+        linkName: "NEWS",
+        link: "/",
+      },
+      {
+        linkName: "PAGES",
+        link: "/",
+      },
+      {
+        linkName: "Contact",
+        link: "/",
+      },
+    ],
+  },
+];
+const footerBottomEn = [
+  {
+    copyRight: "@ COPYRIGHT 2015 EDUCA. ALL RIGHTS RESERVED",
+    pagesLink: [
+      {
+        linkName: "HOME",
+        link: "/",
+      },
+      {
+        linkName: "COURSES",
+        link: "/",
+      },
+      {
+        linkName: "FUTURE STUDENTS",
+        link: "/",
+      },
+      {
+        linkName: "NEWS",
+        link: "/",
+      },
+      {
+        linkName: "PAGES",
+        link: "/",
+      },
+      {
+        linkName: "Contact",
+        link: "/",
+      },
+    ],
+  },
+];
+const footerBottomRu = [
+  {
+    copyRight: "@ COPYRIGHT 2015 EDUCA. ALL RIGHTS RESERVED",
+    pagesLink: [
+      {
+        linkName: "HOME",
+        link: "/",
+      },
+      {
+        linkName: "COURSES",
+        link: "/",
+      },
+      {
+        linkName: "FUTURE STUDENTS",
+        link: "/",
+      },
+      {
+        linkName: "NEWS",
+        link: "/",
+      },
+      {
+        linkName: "PAGES",
+        link: "/",
+      },
+      {
+        linkName: "Contact",
+        link: "/",
+      },
+    ],
+  },
+];
+export {
+  footerDataAz,
+  footerDataEn,
+  footerDataRu,
+  footerBottomAz,
+  footerBottomEn,
+  footerBottomRu,
+};
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,8 +1,9 @@
-
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <main>
+      <section className="container mx-auto flex flex-col justify-between px-3 md:px-1"></section>
+    </main>
+  );
 }
 
-export default Footer
+export default Footer;
